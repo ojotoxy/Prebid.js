f3a20a09ead4e01ba808f4f8d515cba661d71d54
Matt Lane <mlane@appnexus.com>
2016-07-27 17:06:15 -0700

    Implement inital response handler function

--- src/adapters/ut.js
+++ src/adapters/ut.js
@@ -1,9 +1,14 @@
+import { createBid } from '../bidfactory';
+import { addBidResponse } from '../bidmanager';
 import { ajax } from '../ajax';
 import { generateUUID, getSizes } from '../utils';
+const CONSTANTS = require('../constants.json');
 
 const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
 
+// function/module will be renamed
 function UtAdapter() {
+
   function callBids(params) {
     const tags = params.bids.map(bid => {
       let tag = {};
@@ -29,13 +34,36 @@ function UtAdapter() {
     };
 
     const payload = JSON.stringify(request);
-    ajax(ENDPOINT, handleResponse, payload);
+    ajax(ENDPOINT, registerResponse, payload);
   }
 
-  function handleResponse(response, xhr) {
+  function registerResponse(response) {
+    for (const tag of JSON.parse(response).tags) {
+      const ad = tag.ads[0];
+      const bid = createBid(CONSTANTS.STATUS.GOOD);
+      bid.code = 'ut';
+      bid.bidderCode = 'ut';
+      bid.creative_id = ad.creativeId;
+      bid.cpm = ad.cpm;
+      bid.ad = ad.rtb.banner.content;
+      // try {
+      //   const url = ad.rtb.trackers[0].impression_urls[0];
+      //   const tracker = utils.createTrackPixelHtml(url);
+      //   bid.ad += tracker;
+      // } catch (e) {
+      //   utils.logError('Error appending tracking pixel', 'appnexusAst.js:_requestAds', e);
+      // }
+
+      bid.width = ad.rtb.banner.width;
+      bid.height = ad.rtb.banner.height;
+      addBidResponse(tag.tag_id, bid);
+    }
   }
 
   return {callBids};
+
 }
 
+// using this instead of ES2015 equivalent because `export default` compiles to
+// `exports.default`, not `module.exports` as needed in build system
 module.exports = UtAdapter;
--- test/spec/adapters/appnexus_spec.js
+++ test/spec/adapters/appnexus_spec.js
@@ -1,44 +1,33 @@
 import { expect } from 'chai';
+import * as bidmanager from 'src/bidmanager';
 import adapter from 'src/adapters/ut';
 
-describe('AppNexusAdapter', () => {
-
-  it('exists and is a function', () => {
-    expect(adapter).to.exist.and.to.be.a('function');
-  });
-
-  describe('sends bid request', () => {
-
-    const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
-    const params = {
-      "bidderCode": "ut",
-      "requestId": "d3e07445-ab06-44c8-a9dd-5ef9af06d2a6",
-      "bidderRequestId": "7101db09af0db2",
-      "bids": [
-        {
-          "bidder": "ut",
-          "params": {
-            "foo": "bar"
-          },
-          "placementCode": "/19968336/header-bid-tag1",
-          "sizes": [
-            [
-              728,
-              90
-            ],
-            [
-              970,
-              90
-            ]
-          ],
-          "bidId": "84ab500420319d",
-          "bidderRequestId": "7101db09af0db2",
-          "requestId": "d3e07445-ab06-44c8-a9dd-5ef9af06d2a6"
-        }
+const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
+const params = {
+  "bidderCode": "ut",
+  "requestId": "d3e07445-ab06-44c8-a9dd-5ef9af06d2a6",
+  "bidderRequestId": "7101db09af0db2",
+  "bids": [
+    {
+      "bidder": "ut",
+      "params": {
+        "foo": "bar"
+      },
+      "placementCode": "/19968336/header-bid-tag1",
+      "sizes": [
+        [728, 90],
+        [970, 90]
       ],
-      "start": 1469479810130,
-      "timeout": 3000
-    };
+      "bidId": "84ab500420319d",
+      "bidderRequestId": "7101db09af0db2",
+      "requestId": "d3e07445-ab06-44c8-a9dd-5ef9af06d2a6"
+    }
+  ],
+  "start": 1469479810130,
+  "timeout": 3000
+};
+
+describe('AppNexusAdapter', () => {
 
     let xhr;
     let requests;
@@ -53,12 +42,22 @@ describe('AppNexusAdapter', () => {
       xhr.restore();
     });
 
-    it('to ENDPOINT via POST', () => {
-      adapter().callBids(params);
-      expect(requests[0].url).to.equal(ENDPOINT);
-      expect(requests[0].method).to.equal('POST');
-    });
 
+  it('exists and is a function', () => {
+    expect(adapter).to.exist.and.to.be.a('function');
+  });
+
+  it('sends bid request to ENDPOINT via POST', () => {
+    adapter().callBids(params);
+    expect(requests[0].url).to.equal(ENDPOINT);
+    expect(requests[0].method).to.equal('POST');
+  });
+
+  xit('registers a bid response', () => {
+    const addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
+    adapter().callBids(params);
+    // sinon.assert.calledOnce(addBidResponse);
+    bidmanager.addBidResponse.restore();
   });
 
 });
